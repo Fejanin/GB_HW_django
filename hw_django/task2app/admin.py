f0ced6a5f25e5e7faa4dd2e4bbd4a1cfc1ec3107
Shvec Sergei <fejanin@mail.ru>
2024-01-22 20:10:37 +0300

    Task 5 is completed.

--- hw_django/task2app/admin.py
+++ hw_django/task2app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Client, Good, Order
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email']
+
+class GoodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'describe', 'price', 'count']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'client']
+    ordering = ['id']
+    list_filter = ['date']
 
 # Register your models here.
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Good, GoodAdmin)
+admin.site.register(Order, OrderAdmin)
